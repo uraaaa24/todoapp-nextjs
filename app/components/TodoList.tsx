82efb44d4f6057aece1025c539a4c9c0f2bacc54
Yuto Urashima <yuto.urashima24@gmail.com>
2024-05-11 17:46:38 +0900

    TodoListコンポーネントを作成

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,7 +1,16 @@
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <ul className="space-y-3">
+      {/* TODO: サンプルデータを実際のデータに置き換える */}
+      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+        <span>散歩</span>
+        <div className="flex gap-3">
+          <button className="text-green-500">Edit</button>
+          <button className="text-red-500">Delete</button>
+        </div>
+      </li>
+    </ul>
   )
 }
 
